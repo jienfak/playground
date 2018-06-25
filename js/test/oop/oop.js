9a5e6675c1813e93e89ecca71a1b4ef3612a0e93
jienfak <jienfak@protonmail.com>
2018-06-25 14:34:24 +0500

    JS oop changed

--- js/test/oop/oop.js
+++ js/test/oop/oop.js
@@ -1,16 +1,41 @@
 #!/usr/bin/node
 
-var Person = function (name){
-	/* -------------------------- */
-	this.name = name;
-	console.log("Object 'Person' with name '"+this.name+"' was created.");
+// Example of inheritance
+var animal_proto = {
+	eats: true
+};
 
-	this.say = function(string){
-		console.log(this.name+": "+string);
-	}
+var human_proto = {
+	is_stupid: true,
+	name: "Samuel",
+	say: function(string){
+		console.log(this.name + ": " + string);
+	},
+	toString: function(){
+		return this.name ;
+	},
+	String: this.toString
 }
 
+//Inheritance
+Human.__proto__ = animal_proto ;
 
-var pers1 = new Person("sucker");
-pers1.say("You will die");
+console.log("animal_proto.eats = " + animal_proto.eats);
+console.log("animal_proto.is_stupid = " + animal_proto.is_stupid);
 
+console.log("human_proto.eats = " + human_proto.eats);
+console.log("human_proto.is_stupid = "+human_proto.is_stupid);
+
+// we can use this to make classe just like this
+function Human (name, is_stupid) {
+	this.__proto__ = human_proto ; // default values of constructor
+
+	//getting of propery by arguments
+	this.name = name ;
+	this.is_stupid = is_stupid ; 
+}
+
+// getting new object of class 'Human'
+var samuel = new Human ('samuel', false) ;
+samuel.say("You suck!");
+console.log(samuel);
