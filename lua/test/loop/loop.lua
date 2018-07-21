bab1ae48d0409fd32e33f621cafd401eb3d6620e
jienfak <jienfak@protonmail.com>
2018-07-21 10:14:00 +0000

    configuration files

--- lua/test/loop/loop.lua
+++ lua/test/loop/loop.lua
@@ -11,3 +11,15 @@ while i<10 do
 	i = i + 1
 	print(i)
 end
+
+o = {
+	a=1, b=2, c=3,
+	d=4, e=5, f=6,
+	g=7, k=8, o=9
+}
+
+print "Before"
+
+for key, value in pairs(o) do
+	print("o["..key.."]=".."'"..value.."'")
+end
---
+++ sh/sh/conf/arch_install_configs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ ln -s ~/code/scripts/other/opt/jien_Xmodmap	~/.Xmodmap
+ ln -s ~/code/scripts/other/opt/jien_Xresources	~/.Xresources
+ ln -s ~/code/scripts/sh/fish/jien_config.fish	~/.config/fish/config.fish
+ ln -s ~/code/scripts/other/opt/jien_xinitrc	~/.xinitrc
