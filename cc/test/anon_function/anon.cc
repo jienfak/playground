543ab0ad332f9479472c0777cb74756a62311dd6
jien <jienfak@protonmail.com>
2018-07-30 08:48:28 +0300

    configurations changed

--- cc/test/anon_function/anon.cc
+++ cc/test/anon_function/anon.cc
@@ -7,7 +7,7 @@ int main(void){
 	int (*func)(void) = [](/* params */ void) ->/* ret type */ int {
 		cout<<"Anon function!"<< endl;
 		return 123 ;
-	};
+	} ;
 
 	int num = func() ;
 	cout<< num << endl;
--- sh/sh/conf/arch_install_configs.sh
+++ sh/sh/conf/arch_install_configs.sh
@@ -2,5 +2,11 @@
 
  ln -s ~/code/scripts/other/opt/jien_Xmodmap	~/.Xmodmap
  ln -s ~/code/scripts/other/opt/jien_Xresources	~/.Xresources
+
  ln -s ~/code/scripts/sh/fish/jien_config.fish	~/.config/fish/config.fish
+
  ln -s ~/code/scripts/other/opt/jien_xinitrc	~/.xinitrc
+
+ ln -s ~/code/scripts/vim/jien_vimrc		~/.vimrc
+ mkdir ~/.config/nvim
+ ln -s ~/code/scripts/vim/jien_vimrc		~/.config/nvim/init.vim
