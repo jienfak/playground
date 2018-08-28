8d338b6e866c0503e972a6d3b030a332acf248a7
jien <jienfak@protonmail.com>
2018-08-28 18:51:46 +0300

    Arch conf.

--- sh/sh/configuring/arch_install_configs.sh
+++ sh/sh/configuring/arch_install_configs.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
- # directories
+ # Directories.
  mkdir ~/video ~/audio ~/docs ~/temps ~/imgs ~/shared
  rm -f ~/.config/user-dir.dirs
  ln -s ~/code/scripts/other/opt/jien_user-dir.dirs ~/.config/user-dir.dirs
- mkdir ~/code/ ~/progs ~/torrent
+ mkdir ~/code/ ~/progs ~/torrent ~/bluetooth
 
- #X-es
+ # X.
  ln -s ~/code/scripts/other/opt/jien_Xmodmap	~/.Xmodmap
  ln -s ~/code/scripts/other/opt/jien_Xresources	~/.Xresources
 
- #fish
+ # Fish.
  sudo mv /etc/fish/config.fish /etc/fish/config.fish.backup
  sudo ln -s ~/code/scripts/sh/fish/jien_config.fish	/etc/fish/config.fish
 
- #xinit
+ # Xinit.
  ln -s ~/code/scripts/other/opt/jien_xinitrc	~/.xinitrc
 
- #editor
+ # Editor.
 
  sudo mv /etc/vimrc /etc/vimrc.backup
  sudo ln -s ~/code/scripts/vim/jien_vimrc		/etc/vimrc
