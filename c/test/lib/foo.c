2b8424ea4ccd736bd8acff83bd079fdc19a22c77
jienfak <jienfak@protonmail.com>
2018-12-17 23:03:20 +0000

    C-lib example added

---
+++ c/test/lib/foo.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void foo(void){
+	puts("I'm in this fucking library!");
+}
---
+++ c/test/lib/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "foo.h"
+
+int main(int argc, char **argv){
+	puts("It's a fucking test, trying to call 'foo' function!");
+	foo();
+	
+	return 0 ;
+}
