7e3da08a28563c0dd96fa2f0169bf25c35d64d2d
jien <jien@protonmail.com>
2019-03-14 21:33:24 +0500

    ...

--- c/utils/hex2bin/hex2bin.c
+++ c/utils/hex2bin/hex2bin.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
+char hex2char(char a, char b){
+	char a_ret = 0 ;
+	if(('0'<=a)&&(a<='9')){
+		a_ret = a - '0';
+	}else if(('A'<=a)&&(a<='F')){	
+		a_ret = a-'A'+10 ;
+	}
+	a_ret <<= 4 ;
+
+	char b_ret = 0 ;
+	if(('0'<=b)&&(b<='9')){
+		b_ret = b - '0';
+	}else if(('A'<=b)&&(b<='F')){	
+		b_ret = b-'A'+10 ;
+	}
+
+	return a_ret | b_ret ;
+}
+
 
 int main(int argc, char **argv){
-	char buf[sizeof(unsigned int)+1]; buf[sizeof(unsigned int)] = '\0' ;
-	while(scanf("%x", buf)!=EOF){
-		printf("%s", buf);
+	char c1, c2;
+	while((c1=getchar())!=EOF && (c2=getchar())!=EOF){
+		printf("%c", hex2char(c1, c2));
 	}
 	/* Succesful exit. */
 	return 0 ;
