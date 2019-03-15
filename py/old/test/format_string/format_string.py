40e17cd9ea98d96e6642de138a08e7b029d4cadb
jien <jien@protonmail.com>
2019-03-15 19:24:47 +0000

    Python: example with format.

---
+++ py/old/test/format_string/format_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+string1 = "'%s' = here we get string and %d = here we get integer." % ("Hi, that's string", 1456)
+string2 = "%x = here we get hex." % (80)
+print("string1 = "+string1)
+print("string2 = 0x"+string2)
