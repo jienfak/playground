cce45db2f0e90ca276bac3a6477d240ac0ba2f03
jien <jien@protonmail.com>
2019-03-15 18:28:49 +0000

    Changed theme in awesome

--- other/opt/jien_awesome_rc.lua
+++ other/opt/jien_awesome_rc.lua
@@ -62,7 +62,7 @@ end
 -- {{{ Variable definitions.
 -- Themes define colours, icons, font and wallpapers.
 -- xresources, zenburn, default, sky
-beautiful.init(awful.util.get_themes_dir().."zenburn/theme.lua")
+beautiful.init(awful.util.get_themes_dir().."xresources/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 local sys_stat_program = "htop"
