9dc2d8c69e7f43ff650a2295263a7d67aeb5b494
jien <jien@protonmail.com>
2019-03-18 20:03:12 +0000

    Blackarch: added tools list generator in pl.

---
+++ pl/5/utils/blackarch/gen_util_list.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+#
+# Simple script getting list of tools for Blackarch(18 March 2019).
+
+use strict;
+use warnings;
+use feature 'say';
+
+my $url = 'https://blackarch.org/tools.html';
+my $data = `wget --output-document=- $url` ;
+
+# Chores about regexps.
+my $mark = qr/'|\""/ ;
+
+# Categories.
+my @hcats = () ;
+while( $data =~ m{
+			<.*a.*class=$mark?hcat$mark?.*>(.*)</a>
+		}gx
+	){
+	#say $1 ;
+	push @hcats, $1 ;
+}
+
+# Utils.
+my @utils = () ;
+while( $data =~ m{
+		<.*td.*class=$mark?tbl-name$mark?.*>(.*)</td>
+	}gx
+	){
+	#say $1;
+	push @utils, $1 ;
+}
+
+# For chomp.
+$/ = ' ' ;
+for(my $i=0 ; $i<@utils ; ++$i ){	
+	my $string = $utils[$i]." ".$hcats[$i] ;
+	chomp $string;
+	say $string ;
+}
+# Back start chomp function.
+$/ = '\n' ;
+
+exit;
