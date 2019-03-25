f134e705de552e1fc8af1c022ddd14ea32712057
jien <jien@protonmail.com>
2019-03-25 00:46:14 +0000

    profile: Added environment variables file.

--- sh/sh/modules/dirs.sh
+++
@@ -1,2 +0,0 @@
-
-scripts=$HOME/code/scripts
---
+++ sh/sh/modules/helpa.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/sh
+
+helpa(){
+	# Check manual first.
+	man $argv >/dev/null 2>&1
+	if [ $status = 0 ] ; then
+		man $argv
+		echo "Got manual, for $argv."
+		return
+	fi
+
+	# Check info.
+	info --version
+	if [ $status = 0 ]  ; then
+		echo "Got info page for $argv."
+		info $argv	
+		if [ $status = 0 ] ; then
+			return
+		fi
+	fi
+	
+	# Check 'help' options.
+	which $argv > /dev/null 2>&1
+	if [ $status != 0 ] ; then
+		echo "helpa: Program '$argv' not found."
+		return 
+	fi
+	
+	eval $argv -h > /dev/null 2>&1
+	if [ $status = 0 ] ; then 
+		echo "helpa: Worked with '-h' option."
+		eval $argv -h | manpager 2>&1
+		return
+	fi
+
+	eval $argv --help > /dev/null 2>&1
+	if [ $status = 0 ] ; then
+		echo "helpa: Worked with '--help' option."
+		eval $argv --help | manpager 2>&1
+		return
+	fi
+
+	eval $argv -help > /dev/null 2>&1
+	if [ $status = 0 ] ; then
+		echo "helpa: Worked with '-help' option"
+		eval $argv -help | manpager 2>&1
+		return
+	fi
+
+	eval $argv help > /dev/null 2>&1
+	if [ $status = 0 ] ; then
+		echo "helpa: Worked with 'help' option."
+		eval $argv help | manpager 2>&1
+		return
+	fi
+
+	eval $argv > /dev/null 2>&1
+	if [ $status = 0 ] ; then
+		echo "helpa: Worked without any options."
+		eval $argv | manpager 2>&1
+		return
+	fi
+
+	echo "helpa: Could not get any help!"
+}
