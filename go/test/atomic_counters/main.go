49a8c2efdfcf7d135ffa04591f521b42f03e5259
jien <jien@protonmail.com>
2019-04-20 20:44:40 +0000

    go: More examples.

---
+++ go/test/atomic_counters/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "time"
+import "sync/atomic"
+
+func main(){
+	var ops uint64
+
+	for i:=0 ; i<50 ; i++ {
+		go func(){
+			atomic.AddUint64(&ops, 1)
+			time.Sleep(time.Millisecond)
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	ops_final := atomic.LoadUint64(&ops)
+	fmt.Println("ops:", ops_final)
+	
+}
---
+++ go/test/defer1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+import "os"
+
+func main(){
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
+
+func createFile(p string) *os.File {
+	fmt.Println("Creating file...")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File){
+	fmt.Println("Writing...")
+	fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File){
+	fmt.Println("Closing...")
+	f.Close()
+}
